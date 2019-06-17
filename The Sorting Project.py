7bf3e34ac49e54928d9aa6ee8ccb33c737ce4071
YaoMaxVanier2019 <48729830+YaoMaxVanier2019@users.noreply.github.com>
2019-06-17 17:34:14 -0400

    Add files via upload

---
+++ The Sorting Project.py
@@ -0,0 +1,317 @@
+import tkinter as tk
+import csv
+import time
+import math
+from memory_profiler import profile
+import visual_sort as spa
+
+def SORTING():
+
+    back = False 
+
+    #------------------------------------
+    """
+    Reading text file for list
+    """
+    def readlist():
+        ilistO = []
+        temp_lis = []
+        with open ("yagmai.txt") as tf:
+            rtf = csv.reader(tf, delimiter = ",")
+            for line in rtf:
+                for p in range (len(line)):
+                    temp_lis.append(int(line[p]))
+                ilistO.extend(temp_lis)
+                temp_lis = []
+        return ilistO
+
+    def info(ilist, end, start, no,itype):
+        n = len(ilist)
+        print (" ")
+        print ("Time used = ", end - start)
+        print (" ")
+        print ("#O = ", no) #FIND NUMBER OF OPERATIONS AND PUT HERE
+        if itype == 1 :
+            print ("Best Case: ", (n*math.log(n)))
+            print ("Worst Case: ", (n**2))
+            print ("Average Case : ", (n*math.log(n)))
+        elif itype == 2:
+            print ("Best Case: ", (n))
+            print ("Worst Case: ", (n**2))
+            print ("Average Case: ",(n**2))
+        elif itype == 3:
+            print ("Best Case: ", (n))
+            print ("Worst Case: ", (n**2))
+            print ("Average Case: ",(n**2))
+        elif itype == 4:
+            print ("Best Case: ", (n*10))
+            print ("Worst Case: ", (n*10))
+            print ("Average Case: ",(n*10))
+        elif itype == 5:
+            print ("Best Case: ", (n*math.log(n)))
+            print ("Worst Case: ", (n**2))
+            print ("Average Case: ",(n*math.log(n)))
+    #-------------------------------------
+    #Visuals
+
+    def quickV():
+        ilistO = readlist()
+        ilist = ilistO
+
+        spa.visual_graph(1,ilist)
+
+        print (" ")
+        print ("Quick Sort")
+        print (ilistO)
+        
+    def bubbleV():
+        ilistO = readlist()
+        ilist = ilistO
+
+        spa.visual_graph(2,ilist)
+
+        print (" ")
+        print ("Quick Sort")
+        print (ilistO)
+        
+    def insertionV():
+        ilistO = readlist()
+        ilist = ilistO
+
+        spa.visual_graph(3,ilist)
+        
+        print (" ")
+        print ("Quick Sort")
+        print (ilistO)
+        
+    def radixV():
+        ilistO = readlist()
+        ilist = ilistO
+
+        spa.visual_graph(4,ilist)
+        
+        print (" ")
+        print ("Quick Sort")
+        print (ilistO)
+        
+    def mergeV():
+        ilistO = readlist()
+        ilist = ilistO
+
+        spa.visual_graph(5,ilist)
+        
+        print (" ")
+        print ("Quick Sort")
+        print (ilistO)
+
+    #-------------------------------------
+    #No Visuals
+
+    import sorts_no_visuals as snv
+    @profile
+    def quick():
+        ilistO = readlist()
+        ilist = ilistO
+
+        start = time.time()
+        no=snv.quick_sort(ilist,0,len(ilist)-1,0)
+        end = time.time ()
+
+       
+        print (" ")
+        print ("Quick Sort")
+        print (ilist)
+        info(ilist, end, start,no,1) 
+
+    @profile
+    def bubble():
+        ilistO = readlist()
+        ilist = ilistO
+
+        start = time.time()
+        no=snv.bubblesort(ilist)
+        end = time.time ()
+        
+        print (" ")
+        print ("Bubble Sort")
+        print(ilist)
+        info(ilist, end, start,no,2)
+    @profile
+    def insertion():
+        ilistO = readlist()
+        ilist = ilistO
+
+        start = time.time()
+        no= snv.insertionsort(ilist)
+        end = time.time ()
+        
+        print (" ")
+        print ("Insertion Sort")
+        print(ilist)
+        info(ilist, end, start,no,3) 
+    @profile
+    def radix():
+        ilistO = readlist()
+        ilist = ilistO
+        
+        start = time.time()
+        no=snv.radixsort(ilist)
+        end = time.time ()
+
+        print (" ")
+        print ("Radix Sort")
+        print(ilist)
+        info(ilist, end, start,no,4) 
+    @profile    
+    def merge():
+        ilistO = readlist()
+        ilist = ilistO
+
+        start = time.time()
+        no=snv.mergesort(ilist)
+        end = time.time ()
+
+        print (" ")
+        print ("Merge Sort")
+        print(ilist)
+        info(ilist, end, start,no,5) 
+
+    #-------------------------------------
+
+    def lists():
+        ilistO = readlist()
+        print (" ")
+        print ("Original List")
+        print (ilistO)
+
+    def visuals():
+        mainV()
+
+    def no_visuals():
+        mainNV()
+
+    def ram():
+        print ("ram")
+        #insert open ram code here
+        
+    def mainV():
+
+        
+        root = tk.Tk()
+        frame = tk.Frame(root)
+        frame.pack()
+
+        button = tk.Button(frame, 
+                        text="Back", 
+                        fg="red",
+                        command=root.destroy)
+        button.pack(side=tk.LEFT)
+        slogan = tk.Button(frame,
+                        text="Quick Sort",
+                        command=quickV )
+        
+        slogan.pack(side=tk.LEFT)
+        slogan = tk.Button(frame,
+                        text="Bubble Sort",
+                        command=bubbleV )
+        slogan.pack(side=tk.LEFT)
+        slogan = tk.Button(frame,
+                       text="Insertion Sort",
+                        command=insertionV )
+        slogan.pack(side=tk.LEFT)
+        slogan = tk.Button(frame,
+                        text="Radix Sort",
+                        command=radixV )
+        slogan.pack(side=tk.LEFT)
+        slogan = tk.Button(frame,
+                        text="Merge Sort",
+                        command=mergeV )
+        slogan.pack(side=tk.LEFT)
+
+        root.minsize(400,400)
+        root.maxsize(400,400)
+
+    def mainNV():
+
+            
+        root = tk.Tk()
+        frame = tk.Frame(root)
+        frame.pack()
+
+        button = tk.Button(frame, 
+                        text="Back", 
+                        fg="red",
+                        command=root.destroy)
+        button.pack(side=tk.LEFT)
+        slogan = tk.Button(frame,
+                        text="Quick Sort",
+                        command=quick )
+        
+        slogan.pack(side=tk.LEFT)
+        slogan = tk.Button(frame,
+                        text="Bubble Sort",
+                        command=bubble )
+        slogan.pack(side=tk.LEFT)
+        slogan = tk.Button(frame,
+                        text="Insertion Sort",
+                        command=insertion )
+        slogan.pack(side=tk.LEFT)
+        slogan = tk.Button(frame,
+                        text="Radix Sort",
+                        command=radix )
+        slogan.pack(side=tk.LEFT)
+        slogan = tk.Button(frame,
+                        text="Merge Sort",
+                        command=merge )
+        slogan.pack(side=tk.LEFT)
+
+        root.minsize(400,400)
+        root.maxsize(400,400)
+
+
+        
+    def main():
+        root = tk.Tk()
+        frame = tk.Frame(root)
+        frame.pack()
+
+        button = tk.Button(frame, 
+                       text="QUIT", 
+                       fg="red",
+                       command=quit)
+        button.pack(side=tk.LEFT)
+
+        slogan = tk.Button(frame,
+                       text="Print List",
+                       command=lists )
+        slogan.pack(side=tk.LEFT)
+
+        slogan = tk.Button(frame,
+                       text="Visuals",
+                       command=visuals )
+        slogan.pack(side=tk.LEFT)
+
+
+        Oof = tk.Button(frame,
+                       text="No Visuals",
+                       command=no_visuals)
+        Oof.pack(side=tk.LEFT)
+        
+        Oof = tk.Button(frame,
+                       text="Open Ram",
+                       command=ram)
+        Oof.pack(side=tk.LEFT)
+
+
+        root.minsize(500,500)
+        root.maxsize(500,500)
+        root.mainloop()
+    #----------------------------------------------
+    ilistO = readlist()
+    if ilistO == []:
+        print ("please enter list into yagmai.txt text file")
+        print ("with a format like ##,##,##,##")
+    else:
+        print ("Your list is :", ilistO)
+    main()
+SORTING()
--- sorts_no_visuals.py
+++ sorts_no_visuals.py
@@ -3,7 +3,7 @@
 #------------------------------------------------------------------------------
 
 
-def quick_sort (ilist,low,hi):
+def quick_sort (ilist,low,hi,no):
     
     if low < hi:
         
@@ -13,14 +13,17 @@ def quick_sort (ilist,low,hi):
         border = low
         
         for x in range(low,hi+1):
+            no += 1
             if ilist[x] < pivotValue:
                 border = border + 1
                 ilist[x], ilist[border] = ilist[border], ilist[x]
                 
         ilist[low], ilist[border] = ilist[border], ilist[low]
         p = (border)
-        quick_sort(ilist, low, p - 1)
-        quick_sort(ilist, p + 1, hi)
+        quick_sort(ilist, low, p - 1,no)
+        quick_sort(ilist, p + 1, hi,no)
+    return no
+
 def get_pivot(A, low, hi):
     mid = (hi + low) // 2
     pivot = hi
@@ -35,23 +38,23 @@ def get_pivot(A, low, hi):
 #------------------------------------------------------------------------------
 
 def bubblesort(ilist):
-    hi = len(ilist)-1
-    check = True
-    while check == True:
-        checks = False
-        for x in range (0,hi):
-            if ilist[x] > ilist[x+1]:
-                temp = ilist[x]
-                ilist[x] = ilist[x+1]
-                ilist[x+1] = temp
-                check = True
-                checks = True
+    no = 0
+    LOA = len(ilist)-1
+    for i in range(LOA):
+        hoop = 0
+        for j in range(LOA - i):
+            no += 1
+            if ilist[j] > ilist[j+1]:
+                
+                ilist[j], ilist[j+1] = ilist[j+1], ilist[j]
             else:
-                if checks != True:
-                    check = False
-        hi -= 1
+                hoop +=1
+        if hoop == len(ilist)-1:
+            break
+    return no
     
 def insertionsort(ilist):
+    no=0
     y = 1   #y-index starts at 1 instead of 0 - no numbers before index 0
     temp=0  
     while y < len(ilist):   # Loop will stop when y-index is equal to the length of the list
@@ -64,6 +67,7 @@ def insertionsort(ilist):
 			    	temp = ilist[x]
 			    	ilist[x] = ilist[x-1]
 			    	ilist[x-1] = temp
+			    	no+=1
 		    	else:
 		    		check = 1
 		    	x = x - 1
@@ -71,16 +75,18 @@ def insertionsort(ilist):
 		    	if x == 0:
 		    		check =  1
 	    y = y + 1
+    return no
 
 
 #------------------------------------------------------------------------------
 
 
 def radixsort(ilist):
+    no=0
     digit = 0        #set base digit
     maxdigit = 1     #to find biggest digit
     max_num = max(ilist)
-    while max_num > 10**maxdigit:
+    while max_num >= 10**maxdigit:
     	maxdigit += 1
 			        
     while digit < maxdigit:
@@ -89,7 +95,8 @@ def radixsort(ilist):
     		bucket.setdefault (x,[])     #set each bucket from base 0-9
     	for num in ilist:
     		radix = int((num/(10**digit)%10))  #find its radix(number at the digit that's being checked)
-    		bucket[radix].append(num)  #arrange each number in to its bucket
+    		bucket[radix].append(num)
+    		no +=1
 			
     	index=0
     	for check in range(10):
@@ -99,43 +106,48 @@ def radixsort(ilist):
 					
     				index += 1        #in the order from the bucket
     	digit += 1   #loop stop when check digit reaches maxdigit
-
-
-
+    return no
 #------------------------------------------------------------------------------
 
-
-def mergesort(alist):
+def mergesort (alist):
+    no=mergesort2 (alist, 0)
+    return no
+def mergesort2(alist,no):
     if len(alist)>1:            
         mid =len(alist)//2       
         lefthalf =alist[:mid]   
         righthalf=alist[mid:]    
 
-        mergesort(lefthalf)      
+        mergesort2(lefthalf,no)      
     
-        mergesort(righthalf)   
+        mergesort2(righthalf,no)   
         
         l=0                     
         r=0                      
         a=0                    
 
-        while l < len(lefthalf) and r < len(righthalf):     
+        while l < len(lefthalf) and r < len(righthalf):
+            no +=1
             if lefthalf[l] < righthalf[r]:                  
                 alist[a] = lefthalf[l]                      
                 l = l + 1                                   
-
+                
             else :
                 alist[a] = righthalf[r]
                 r = r + 1
+                no+=1
             a = a + 1                                       
             
         while l < len(lefthalf):        
             alist[a] = lefthalf[l]
             l = l + 1
             a = a + 1
-
+            no += 1
+            
 
         while r < len(righthalf):          
             alist[a] = righthalf[r]
             r = r + 1
             a = a + 1
+            no+=1
+    return no
